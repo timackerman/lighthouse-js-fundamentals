bf2d84c957b40e5595160a25493519db0214324f
Tim Ackerman <timackerman@gmail.com>
2020-10-22 03:04:17 +0000

    more function examples

--- hello.js
+++ hello.js
@@ -5,7 +5,27 @@ const sayHello = function() {
 sayHello();
 */
 
+/*
 const sayHello = function(name) {
   console.log("Hello " + name);
 }
 sayHello("Tim");
+*/
+
+/*
+const sayHelloToConsole = function (name) {
+  console.log("Hello " + name);
+}
+sayHelloToConsole("John");
+*/
+// outputs "Hello John"
+
+/*
+const returnSayHello = function (name) {
+  return "Hello " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
+*/
+// returns the string "Hello John" to a variable
+
