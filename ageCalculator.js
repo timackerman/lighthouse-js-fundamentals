a98ccb5a3ab474ce8a468033ee52b93bf75afc08
Tim Ackerman <timackerman@gmail.com>
2020-10-24 03:19:52 +0000

    calculate the age of someone if only their year of birth is known

---
+++ ageCalculator.js
@@ -0,0 +1,5 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return (name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
