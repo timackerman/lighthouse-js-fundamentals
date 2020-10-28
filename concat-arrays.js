414abdffc238cdc30df133937035cdbcd2101a1a
Tim Ackerman <timackerman@gmail.com>
2020-10-28 03:52:33 +0000

    concat 2 arrays without using concat() function

---
+++ concat-arrays.js
@@ -0,0 +1,11 @@
+function concat(fromArray, toArray) {
+  for (let i = 0, len = toArray.length; i < len; i++) {
+    fromArray.push(toArray[i]);
+  }
+  return fromArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
