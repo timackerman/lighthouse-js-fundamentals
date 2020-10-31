f999caf254988700ea0a46f17be19716c2eb01ad
Tim Ackerman <timackerman@gmail.com>
2020-10-31 03:42:23 +0000

    function to count new items added and output object

---
+++ trash-to-treasure.js
@@ -0,0 +1,11 @@
+const smartGarbage = function(trash, bins) {
+  let updatedBins = bins;
+  if (trash === "waste") {
+    updatedBins.waste++;
+  } else if (trash === "recycling") {
+    updatedBins.recycling++;
+  } else if (trash === "compost") {
+    updatedBins.compost++;
+  }
+  return updatedBins;
+};
